b359b644f067702a6cc561b63b9cafbffe4391f1
Giuseppe Conticchio <giuseppe.conticchio@macnil.local>
2021-04-19 16:26:01 +0200

    added css style in jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils.js'
 import CartIcon from '../cart-icon/cart-icon.component.jsx';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component.jsx';
-import {selectCartHidden} from '../../redux/cart/cart.selectors';
-import {selectCurrentUser} from '../../redux/user/user.selectors';
-import '../header/header.styles.scss';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import {HeaderContainer, OptionsContainer, OptionDiv, OptionLink, LogoContainer} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'> {/*Link è il tag per collegare una voce all'url in qst caso alla homepage*/}
+    <HeaderContainer>
+        <LogoContainer to='/'> {/*Link è il tag per collegare una voce all'url in qst caso alla homepage*/}
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/contact'>
+            </OptionLink>
+            <OptionLink to='/contact'>
                 CONTACT
-            </Link>{/*se l'utente è loggato allora visualizza in header testo SIGN OUT e se viene cliccato richiama il metodo signout per uscire*/}
+            </OptionLink>{/*se l'utente è loggato allora visualizza in header testo SIGN OUT e se viene cliccato richiama il metodo signout per uscire*/}
             {
                 currentUser ?
-                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                    <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
                     :
-                    <Link className='option' to='/signin'>SIGN IN</Link>
+                    <OptionLink to='/signin'>SIGN IN</OptionLink>
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         { //se hidden è falso allora devi visualizzare il popup del carrello altrimenti null
             hidden ? null :
                 <CartDropdown />
         }
-    </div>
+    </HeaderContainer>
 );
 
 //metodo che collega i due stati alla barra header: lo stato di signout e lo stato del popup del carrello
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
 
+import { HomePageContainer } from './homepage.styles';
 /*metodo contenitore homepage  */
 const HomePage = () => (
-    <div className='homepage'>
-        <Directory/> {/*oggetto della classe che definisce le schede menu visibili nella homepage  */}
-    </div>
+    <HomePageContainer>
+        <Directory /> {/*oggetto della classe che definisce le schede menu visibili nella homepage  */}
+    </HomePageContainer>
 );
 
 export default HomePage;

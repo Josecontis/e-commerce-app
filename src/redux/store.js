8f4161aff2895a08ebebc5b5c5f01e01f4857eb1
Giuseppe Conticchio <giuseppe.conticchio@macnil.local>
2021-04-19 12:48:04 +0200

    logger displayed only in dev mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,14 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger]; //vettore di logger visibili in console che rappresentano le azioni effettate dall'utente
+//il logger è un middleware che prende le azioni e le visualizza nella console log
+const middlewares = []; //vettore di logger visibili in console che rappresentano le azioni effettate dall'utente
 
+if(process.env.NODE_ENV === 'development'){ //se la modalità dell app è in sviluppatore allora fai vedere i log
+    middlewares.push(logger);
+}
+
+//middleware sono degli intermediari che prendono le azioni e fanno qualcosa restituendo altre azioni
 export const store = createStore(rootReducer, applyMiddleware(...middlewares)) //...middlewares indica i singoli elementi del vettore passati come param
 
 export const persistor = persistStore(store);
